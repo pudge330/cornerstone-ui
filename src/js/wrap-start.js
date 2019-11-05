1693f84838ef5aec8922156c2517fe574c412a25
Brandon <git@brandonagarcia.me>
2019-11-05 02:41:16 -0500

    General updates, bug fixes, etc.

--- src/js/wrap-start.js
+++ src/js/wrap-start.js
@@ -1,15 +1,22 @@
 (function (root, factory) {
-	var noConflict = root.Cornerstone_NoConflict !== 'undefined' && root.Cornerstone_NoConflict;
+    var dependencies = Object.assign(
+        { amd: {}, common: {} },
+        typeof root.Cornerstone_Dependencies !== 'undefined'
+            ? root.Cornerstone_Dependencies
+            : {}
+    );
+    dependencies.amd = Object.assign({ bglib: 'bglib', jquery: 'jquery' }, dependencies.amd);
+    dependencies.common = Object.assign({ bglib: 'bglib', jquery: 'jquery' }, dependencies.common);
     if (typeof define === 'function' && define.amd) {
-        define(['bglib', 'jquery'], factory);
+        define([dependencies.amd.bglib, dependencies.amd.jquery], factory);
     } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory(require('bglib'), require('jquery'));
+        module.exports = factory(require(dependencies.common.bglib), require(dependencies.common.jquery));
     } else {
-        root.Cornerstone = factory(root.bglib);
+        root.Cornerstone = factory(root.bglib, root.jQuery);
         if (!noConflict) {
             root.CS = root.Cornerstone;
         }
     }
-}(global = typeof self !== 'undefined' ? self : this, function (_bglib) {
+}(global = typeof self !== 'undefined' ? self : this, function (_bglib, jQuery) {
 var Sortable = global.Sortable !== 'undefined' ? global.Sortable : null;
 var jQuery = global.jQuery !== 'undefined' ? global.jQuery : null;
